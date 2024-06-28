6da260e875c819dc1ce65b5a81490f5bc69165e1
JuneSukKim <junesuk04@hufs.ac.kr>
2024-06-28 20:57:50 +0900

    recipe

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./component/Header/Navbar";
 import Footer from "./component/Header/Footer";
 import Main from "./component/main/main";
 import Loading from "./component/loading/loading";
+import MY from "./component/My/myPage";
+import Recipe from "./component/recipe/page/Recmain";
+
 
 import "../src/App.css";
 import Purchase from "./component/main/purchase";
@@ -18,6 +21,8 @@ function App() {
             <Route path="/" element={<Main />} />
             <Route path="/loading" element={<Loading />} />
             <Route path="/purchase" element={<Purchase />} />
+            <Route path="/mypage" element={<MY />} />
+            <Route path="/Recmain" element={<Recipe />}/>
           </Routes>
           <Footer />
         </div>
---
+++ src/component/My/myPage.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import styles from "../css/myPage.module.css";
+import logo from "../img/logoCircle.svg";
+
+export default function myPage() {
+  return (
+    <>
+      <div>
+        <h1>My</h1>
+        <div></div>
+      </div>
+    </>
+  );
+}
