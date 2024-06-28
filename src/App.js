522be1ee720408959b350736b42d7ca52c122e18
sungwonnoh <jessica20730@naver.com>
2024-06-29 02:55:20 +0900

    feat: 공구 deatil

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ import Main from "./component/main/main";
 import Loading from "./component/loading/loading";
 import MY from "./component/My/myPage";
 import SignUp from "./component/My/signUp";
-import Recipe from "./component/recipe/page/Recmain";
 import Login from "./component/My/loginPage";
 import MyInformation from "./component/My/myInformation";
+import Recipe from "./component/recipe/page/Recmain";
 
 import "../src/App.css";
 import Purchase from "./component/purchase/purchase";
 import MakeProom from "./component/purchase/makeProom";
 import MakeForm from "./component/purchase/makeForm";
-import View from "./component/recipe/page/Recview";
-import Write from "./component/recipe/page/Recwrite";
+import Detailpurchase from "./component/purchase/detailpurchase";
 
 function App() {
   return (
@@ -33,9 +32,8 @@ function App() {
             <Route path="/purchase" element={<Purchase />} />
             <Route path="/purchase/:id" element={<MakeProom />} />
             <Route path="/purchase/form" element={<MakeForm />} />
+            <Route path="/purchase/detail" element={<Detailpurchase />} />
             <Route path="/recipe" element={<Recipe />} />
-            <Route path="/view" element={<View />} />
-            <Route path="/write" element={<Write />} />
           </Routes>
         </div>
         <Footer />
--- src/component/purchase/detailpurchase.js
+++ src/component/purchase/detailpurchase.js
@@ -2,7 +2,33 @@ import styles from "../css/detailpurchase.module.css";
 export default function Detailpurchase() {
   return (
     <>
-      <div></div>
+      <div className={styles.detailContainer}>
+        <h3>공동구매</h3>
+        <div className={styles.box}>
+          <h4>구매하기</h4>
+          <a
+            href="https://www.naver.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            🔗 구매 링크 바로가기
+          </a>
+          <div className={styles.people}>공동구매자 모집 현황 3/5</div>
+          <div className={styles.deadline}>
+            <div>마감기한</div>
+            <div> 2024.06.28</div>
+          </div>
+          <div className={styles.amount}>
+            <div>1/n 금액 4320</div>
+          </div>
+          <div className={styles.bankaccount}>
+            <div>대표 계좌</div>
+            <div className={styles.date}>농협 352-1578-4929-63</div>
+          </div>
+          <div className={styles.sale}>-5650원 할인!</div>
+          <button>공동구매하기</button>
+        </div>
+      </div>
     </>
   );
 }
